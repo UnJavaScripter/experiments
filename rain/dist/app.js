07d0ac318cd1ae02135713563f54bef79473a435
UnJavaScripter <diegocoyt@gmail.com>
2020-04-29 00:20:08 +0200

    refresh gitignore

---
+++ rain/dist/app.js
@@ -0,0 +1,95 @@
+import { Particle } from './particle.js';
+class App {
+    constructor() {
+        this.gameOver = false;
+        this.particles = [];
+        this.canvas = document.getElementById('canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.canvas.addEventListener('pointerdown', (e) => {
+            if (this.gameOver) {
+                this.init();
+                this.gameOver = false;
+            }
+            else {
+                this.addElem({
+                    x: e.x,
+                    y: e.y,
+                    width: 4,
+                    height: 4,
+                    color: 'red',
+                    mass: 1
+                });
+            }
+        });
+        this.particlesAmount = 100;
+        this.init();
+    }
+    init() {
+        let raf;
+        const gameLoop = () => {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            if (this.particles.length < 1000) {
+                this.createSeed(this.particlesAmount);
+            }
+            for (let i = 0; i < this.particles.length; i++) {
+                const drop1 = this.particles[i];
+                drop1.update();
+                if (drop1.landed) {
+                    this.particles.splice(i, 1);
+                }
+                for (let j = i + 1; j < this.particles.length; j++) {
+                    const drop2 = this.particles[j];
+                    const distanceH = this.getHypotenuse(drop1, drop2);
+                    if (distanceH <= 10) {
+                        this.particles.splice(j, 1);
+                        drop1.x = drop1.y > drop2.y ? drop1.x : drop2.x,
+                            drop1.y = drop1.y > drop2.y ? drop1.y : drop2.y,
+                            drop1.mass = drop1.mass + drop2.mass;
+                    }
+                }
+            }
+            if (this.gameOver) {
+                cancelAnimationFrame(raf);
+            }
+            else {
+                raf = requestAnimationFrame(gameLoop);
+            }
+        };
+        raf = requestAnimationFrame(gameLoop);
+        this.startTime = performance.now();
+    }
+    getHypotenuse(particle1, particle2) {
+        const distanceX = Math.abs((particle1.x + (particle1.width / 2)) - (particle2.x + (particle2.width / 2)));
+        const distanceY = Math.abs((particle1.y + (particle1.height / 2)) - (particle2.y + (particle2.height / 2)));
+        return Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+    }
+    createSeed(seedAmount) {
+        const n = 0;
+        const generatedXValues = new Set();
+        function getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min)) + min || 1;
+        }
+        const xVal = getRandomInt(1, this.canvas.width - 10);
+        if (generatedXValues.has(xVal)) {
+            return;
+        }
+        generatedXValues.add(xVal);
+        this.addElem({
+            x: xVal,
+            y: getRandomInt(1, 10),
+            width: 4,
+            height: 4,
+            color: '#405086ff'
+        });
+        if (n < seedAmount - 1) {
+            this.createSeed(seedAmount - 1);
+        }
+    }
+    addElem(particleProps) {
+        this.particles.push(new Particle(this.ctx, particleProps));
+    }
+}
+new App();
+//# sourceMappingURL=app.js.map
---
+++ rain/dist/particle.js
@@ -0,0 +1,38 @@
+export class Particle {
+    constructor(ctx, props) {
+        this.ctx = ctx;
+        this.x = props.x;
+        this.y = props.y;
+        this.width = props.width;
+        this.height = props.height;
+        this.color = props.color || 'azure';
+        this.direction = props.direction || { x: 0, y: 1 };
+        this.mass = props.mass || 10;
+        this.landed = false;
+        this.gravity = 9.8;
+        this.velocity = {
+            x: 0,
+            y: 0
+        };
+        this.draw();
+    }
+    draw() {
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x, this.y, this.width, this.height);
+        this.ctx.fill();
+    }
+    update() {
+        if (this.y + this.height > this.ctx.canvas.height || this.y <= 0) {
+            this.landed = true;
+        }
+        else {
+            const t = (Math.pow(this.x, 2) * this.gravity) / 2;
+            this.x = (this.x + (this.direction.x * this.velocity.x));
+            this.y += (this.mass * this.gravity) / 100;
+            // this.ctx.font = '10px serif';
+            // this.ctx.fillText(Math.round(Math.floor(this.mass)).toString(), this.x, this.y);
+        }
+        this.draw();
+    }
+}
+//# sourceMappingURL=particle.js.map
